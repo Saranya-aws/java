192c135a614c1d298170777d76c12cadd0a2b65d
Saranya-aws <rsaranyaa.ece@gmail.com>
2025-12-04 20:07:30 +0800

    Update HelloWorld.java

--- java/src/main/java/com/saranya/HelloWorld.java
+++ java/src/main/java/com/saranya/HelloWorld.java
@@ -1,8 +1,9 @@
-package com.saranya;
-
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Docker!");
+        // Keep the container running
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
+
 
